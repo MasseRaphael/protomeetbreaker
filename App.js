e224806319993a55cc2eb256d0a0cd1b52de9e01
MasseRaphael <38262822+MasseRaphael@users.noreply.github.com>
2020-12-14 20:54:38 +0100

    randomisation des couleurs

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import * as React from "react";
 import { View, Text } from "react-native";
 
 export default function App() {
+  var colors = ["#262484", "#eb5674", "#f1b710", "#37b491"]
+  var color = colors[Math.floor(Math.random() * Math.floor(4))]
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: color,
       }}
     >
       <Text>Universal React with Expo</Text>
