89daf71f6b597fefb9e235a9337e50609a1e75b4
MasseRaphael <38262822+MasseRaphael@users.noreply.github.com>
2020-12-15 13:38:45 +0100

    suppression bouton start

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import * as React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
-import ButtonStart from "./component/ButtonStart";
 
 export default function App() {
-  var colors = ["#262484", "#eb5674", "#f1b710", "#37b491"]
-  var color = colors[Math.floor(Math.random() * Math.floor(4))]
+  var colors = ["#262484", "#eb5674", "#f1b710", "#37b491"];
+  var color = colors[Math.floor(Math.random() * Math.floor(4))];
   return (
     <View
       style={{
